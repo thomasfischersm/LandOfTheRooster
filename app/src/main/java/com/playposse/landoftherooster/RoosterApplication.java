84b27b0906e63ee9ba7e0e9a68f86b721834b066
thomasfischersm <thomas@playposse.com>
2018-12-09 17:54:50 -0500

    Updated library versions

--- app/src/main/java/com/playposse/landoftherooster/RoosterApplication.java
+++ app/src/main/java/com/playposse/landoftherooster/RoosterApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.multidex.MultiDexApplication;
 import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
@@ -22,7 +23,7 @@ import static com.playposse.landoftherooster.activity.KingdomActivity.PRODUCT_FL
 /**
  * Implementation of {@link Application}.
  */
-public class RoosterApplication extends Application {
+public class RoosterApplication extends MultiDexApplication {
 
     private static final String LOG_TAG = RoosterApplication.class.getSimpleName();
 
